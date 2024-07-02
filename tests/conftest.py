54f0e64b9f4024c2057e8d92c7231490cf622940
ByteYJ <145717481+ByteYJ@users.noreply.github.com>
2024-07-02 11:49:23 -0400

    Revert "Access azure data"

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from pathlib import Path
-
-@pytest.fixture
-def datadir(request):
-    # Path to the directory containing test data
-    test_data_dir = Path(__file__).parent / 'data'
-    return test_data_dir
