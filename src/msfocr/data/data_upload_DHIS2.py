b02a2cedaecb4c7d20ea768692b64862378c1d57
rdziewietin <rdzie14@gmail.com>
2024-07-03 14:47:28 -0400

    Move updated data_upload_DHIS2.py to this repo

--- src/msfocr/data/data_upload_DHIS2.py
+++ src/msfocr/data/data_upload_DHIS2.py
@@ -23,70 +23,23 @@ def configure_DHIS2_server(config_path = "settings.ini"):
 # /api/categoryOptions?fields=name'
 # [categories, categoryOptions, dataSets, organisationUnits, dataElements]
 
-# Get UIDS for dataSet, period, orgUnit
-def getUID(item_type, search_items):
+# Get all UIDs from list for dataSet, period, orgUnit
+# Known Words in dataSet
+def getAllUIDs(item_type, search_items):
     filter_param = 'filter=' + '&filter='.join([f'name:ilike:{term}' for term in search_items])
 
     url = f'{DHIS2_SERVER_URL}/api/{item_type}?{filter_param}'
     response = requests.get(url, auth=(DHIS2_USERNAME, DHIS2_PASSWORD))
     if response.status_code == 401:
         raise ValueError("Authentication failed. Check your username and password.")
+    response.raise_for_status()
     
     data = response.json()
     items = data[item_type]
     print(f"{len(data[item_type])} matches found for {search_items}")
-    print(items)
+    if len(items) > 0:
+        print(items[0])
 
-    id = items[0]['id']
+    id = [(item['displayName'], item['id']) for item in items]
 
     return id
-
-
-# # dataSets, organisationUnits, period, categoryCombos
-# search_items_dataSet = ['Kutupalong', 'Sunday', 'Balukhali']
-# search_items_orgUnit = ['General OPD']
-# search_items_categoryCombo = ['<5y', 'Resident']
-# search_items_dataElements = ['Available consultation days']
-
-# item_type = 'dataSets'
-# dataSet_id = getUID(item_type, search_items_dataSet)
-# item_type = 'organisationUnits'
-# orgUnit_id = getUID(item_type, search_items_orgUnit)
-# item_type = 'dataElements'
-# dataElement_id = getUID(item_type, search_items_dataElements)
-# # item_type = 'categoryOptions'
-# # categoryCombos_id = getUID(item_type, search_items_categoryCombo)
-# period = '2024-06-16P7D'
-
-# data_payload = {
-#     "dataSet": dataSet_id,
-#     "period": period,
-#     "orgUnit": orgUnit_id,
-#     "dataValues": [
-#         {"dataElement": dataElement_id, "value": 5}
-#     ]
-# }
-
-# # Construct the URL for the data value set endpoint
-# data_value_set_url = f'{DHIS2_Test_Server_URL}/api/dataValueSets?dryRun=true'
-# # Send the POST request with the data payload
-# response = requests.post(
-#     data_value_set_url,
-#     auth=(dhis2_username, dhis2_password),
-#     headers={'Content-Type': 'application/json'},
-#     data=json.dumps(data_payload)
-# )
-
-# # Check the response status
-# if response.status_code == 200:
-#     print('Data entry dry run successful')
-#     print('Response data:')
-#     print(response.json())
-# else:
-#     print(f'Failed to enter data, status code: {response.status_code}')
-#     print('Response data:')
-#     print(response.json())
-
-# search_items_categoryCombo = ['Polio (OPV) 1 (from 6 wks)']
-# item_type = 'dataElements'
-# categoryCombos_id = getUID(item_type, search_items_categoryCombo)
