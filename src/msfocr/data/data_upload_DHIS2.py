3b642b76c5e68f7de9a5e03ccbfd80aacc198ff1
rdziewietin <rdzie14@gmail.com>
2024-07-10 15:33:00 -0400

    Added methods to find org unit children and data sets

--- src/msfocr/data/data_upload_DHIS2.py
+++ src/msfocr/data/data_upload_DHIS2.py
@@ -43,3 +43,46 @@ def getAllUIDs(item_type, search_items):
     uid = [(item['displayName'], item['id']) for item in items]
 
     return uid
+
+
+def getOrgUnitChildren(uid):
+    """
+    Searches DHIS2 for all the direct children of an organization unit.
+    :param uid: String of organization unit UID
+    :return: List of (org unit child name, org unit child data sets))
+    """
+    url = f'{DHIS2_SERVER_URL}/api/33/organisationUnits/{uid}?includeChildren=true'
+    
+    response = requests.get(url, auth=(DHIS2_USERNAME, DHIS2_PASSWORD))
+    if response.status_code == 401:
+        raise ValueError("Authentication failed. Check your username and password.")
+    response.raise_for_status()
+    
+    data = response.json()
+    items = data['organisationUnits']
+    children = [(item['name'], item['dataSets']) for item in items if item['id'] != uid]
+    
+    return children
+
+def getDataSets(data_sets_uids):
+    """
+    Searches DHIS2 for every data set given in a list.
+    :param data_sets_uids: List of data set objects retrieved from DHIS2
+    :return: List of (data set name, data set id))
+    """
+    data_sets = []
+    
+    for uid_obj in data_sets_uids:
+        uid = uid_obj['id']
+        url = f'{DHIS2_SERVER_URL}/api/33/dataSets/{uid}'
+        
+        response = requests.get(url, auth=(DHIS2_USERNAME, DHIS2_PASSWORD))
+        if response.status_code == 401:
+            raise ValueError("Authentication failed. Check your username and password.")
+        response.raise_for_status()
+        
+        data = response.json()
+        data_set = (data['name'], data['id'])
+        data_sets.append(data_set)
+        
+    return data_sets
